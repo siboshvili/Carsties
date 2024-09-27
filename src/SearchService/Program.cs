27aa823471d2649fb910d7fdf10b95e7980114b3
siboshvili <siboshvili@gmail.com>
2024-09-27 16:05:35 +0400

    Adding Http communication to get the data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,30 +1,35 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
-using SearchService.Models;
-using SearchService.Properties;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipline
-
-app.UseAuthentication();
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
+    await Policy.Handle<TimeoutException>()
+        .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(10))
+        .ExecuteAndCaptureAsync(async () => await DbInitializer.InitDb(app));
 
-    Console.WriteLine(e);
-}
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
