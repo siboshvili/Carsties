720d1e4e5b2629be185352dc0034b9b407390f53
siboshvili <siboshvili@gmail.com>
2024-10-09 17:37:03 +0400

    Styling the auction cards.

--- frontend/web-app/tailwind.config.ts
+++ frontend/web-app/tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
 export default config;
