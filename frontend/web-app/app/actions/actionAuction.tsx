072e988782ee7e61584e4410dd4e8d18ca23a91b
siboshvili <siboshvili@gmail.com>
2024-10-10 15:17:42 +0400

    Adding a set page size option.

--- frontend/web-app/app/actions/actionAuction.tsx
+++ frontend/web-app/app/actions/actionAuction.tsx
@@ -3,10 +3,11 @@
 import { Auctions, PagedResult } from "@/types";
 
 export async function getData(
-  pageNumber: number = 1
+  pageNumber: number = 1,
+  pageSize: number
 ): Promise<PagedResult<Auctions>> {
   const res = await fetch(
-    `http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`
+    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
   );
 
   if (!res.ok) throw new Error("Failed to fetch data");
