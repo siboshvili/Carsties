cc3703f11080d11b835e45b9036ec0fe9229a22a
siboshvili <siboshvili@gmail.com>
2024-10-10 12:36:22 +0400

    Adding loading to the images.

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import CountdownTimer from "./CountdownTimer";
+import CarImage from "./CarImage";
 
 type Props = {
   auction: any;
@@ -8,17 +9,10 @@ type Props = {
 
 export default function AuctionCard({ auction }: Props) {
   return (
-    <a href="#">
+    <a href="#" className="group">
       <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
         <div>
-          <Image
-            src={auction.imageUrl}
-            alt="image"
-            fill
-            priority
-            className="object-cover"
-            sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 25vw"
-          />
+          <CarImage imageUrl={auction.imageUrl} />
           <div className="absolute bottom-2 left-2">
             <CountdownTimer auctionEnd={auction.auctionEnd} />
           </div>
