cf714884d1f0193bfe503f12cda8b64ba94de9d7
siboshvili <siboshvili@gmail.com>
2024-10-14 16:55:55 +0400

    Getting the session details in the client.

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
 import { Auction, PagedResult } from "@/types";
 import AppPagination from "../components/AppPagination";
-import { getData } from "../actions/actionAuction";
+import { getData } from "../actions/auctionActions";
 import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { useShallow } from "zustand/react/shallow";
