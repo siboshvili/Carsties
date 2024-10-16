48bb5fe8660779ad928b94e2a360b9d23bf00750
siboshvili <siboshvili.andro@gmail.com>
2024-10-16 18:23:52 +0400

    Adding the edit auction page.

--- frontend/web-app/app/auctions/update/[id]/page.tsx
+++ frontend/web-app/app/auctions/update/[id]/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Heading from "@/app/components/Heading";
+import React from "react";
+import AuctionForm from "../../AuctionForm";
+import { getDetailedViewData } from "@/app/actions/auctionActions";
 
-export default function Update({params}: {id: string}){
-    return (
-        <div>Update for {params.id}</div>
-    )
+export default async function Update({ params }: { id: string }) {
+  const data = await getDetailedViewData(params.id);
+  return (
+    <div className="mx-auto mx-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Upadate your auction"
+        subtitle="Please update the details of your car"
+      />
+      <AuctionForm auction={data} />
+    </div>
+  );
 }
