b934c5b49f2736c9246e42d65f47afd068892387
siboshvili <siboshvili.andro@gmail.com>
2024-10-23 11:44:38 +0400

    Adding a toast for an auction finished event.

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,15 +1,15 @@
 import { getDetailedViewData } from "@/app/actions/auctionActions";
+import Heading from "@/app/components/Heading";
 import React from "react";
 import CountdownTimer from "../../CountdownTimer";
 import CarImage from "../../CarImage";
 import DetailedSpecs from "./DetailedSpecs";
-import Heading from "@/app/components/Heading";
 import { getCurrentUser } from "@/app/actions/authActions";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
 import BidList from "./BidList";
 
-export default async function Details({ params }: { id: string }) {
+export default async function Details({ params }: { params: { id: string } }) {
   const data = await getDetailedViewData(params.id);
   const user = await getCurrentUser();
 
@@ -20,19 +20,23 @@ export default async function Details({ params }: { id: string }) {
           <Heading title={`${data.make} ${data.model}`} />
           {user?.username === data.seller && (
             <>
-              <EditButton id={data.id} /> <DeleteButton id={data.id} />
+              <EditButton id={data.id} />
+              <DeleteButton id={data.id} />
             </>
           )}
         </div>
+
         <div className="flex gap-3">
           <h3 className="text-2xl font-semibold">Time remaining:</h3>
           <CountdownTimer auctionEnd={data.auctionEnd} />
         </div>
       </div>
+
       <div className="grid grid-cols-2 gap-6 mt-3">
         <div className="w-full bg-gray-200 aspect-h-10 aspect-w-16 rounded-lg overflow-hidden">
           <CarImage imageUrl={data.imageUrl} />
         </div>
+
         <BidList user={user} auction={data} />
       </div>
 
