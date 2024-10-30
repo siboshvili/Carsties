588e772d16ba8385e373bd49db1f83c98d33a4c9
siboshvili <siboshvili.andro@gmail.com>
2024-10-30 12:30:08 +0400

    Fixing the identity server issues in docker compose.

--- frontend/web-app/app/api/auth/signin/page.tsx
+++ frontend/web-app/app/api/auth/signin/page.tsx
@@ -1,21 +1,19 @@
-// import EmptyFilter from "@/app/components/EmptyFilter";
-// import React from "react";
+import EmptyFilter from "@/app/components/EmptyFilter";
+import React from "react";
 
-// export default function page({
-//   searchParams,
-// }: {
-//   searchParams: { callbackUrl: string };
-// }) {
-//   return (
-//     <EmptyFilter
-//       title="You need to be logged in to do that"
-//       subtitle="Please click below to sing in"
-//       showLogin
-//       callbackUrl={searchParams.callbackUrl}
-//     />
-//   );
-// }
+type searchParams = Promise<{ callbackUrl: string }>;
 
-export default function page() {
-  return "signin";
+export default async function page({
+  searchParams,
+}: {
+  searchParams: searchParams;
+}) {
+  return (
+    <EmptyFilter
+      title="You need to be logged in to do that"
+      subtitle="Please click below to sing in"
+      showLogin
+      callbackUrl={(await searchParams).callbackUrl}
+    />
+  );
 }
