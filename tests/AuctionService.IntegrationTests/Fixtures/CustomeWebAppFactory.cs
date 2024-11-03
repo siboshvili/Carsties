6858913653cce09ec014421820c45b1f52b2f03d
siboshvili <siboshvili.andro@gmail.com>
2024-11-03 14:27:06 +0400

    Integration testing setup.

---
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomeWebAppFactory.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AuctionService.IntegrationTests.Fixtures;
+
+public class CustomeWebAppFactory : WebApplicationFactory<Program>
+{
+
+}
